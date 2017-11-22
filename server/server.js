5d28ed79d035b71e6dded0dbe78355027f155bc4
Bala Konda Reddy D <balakonda.d@gmail.com>
2017-11-22 20:24:08 +0530

    Socket Added

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const PORT = process.env.PORT || 3000;
+
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+	console.log('New User Connected');
 
-app.listen(PORT, () => {
+	socket.on('disconnect', () => {
+		console.log('User disconnected');
+	});
+});
+
+
+
+server.listen(PORT, () => {
 	console.log('App is running at ' + PORT);
 });
